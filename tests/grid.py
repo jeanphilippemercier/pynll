8d10b06dc0507c359ccb7bfb4d1a9466f2f1a5f9
jpmercier <jpmercier01@gmail.com>
2021-02-08 15:51:04 -0500

    adding a test for the grid

--- tests/grid.py
+++ tests/grid.py
@@ -16,3 +16,20 @@
     GNU Lesser General Public License, Version 3
     (http://www.gnu.org/copyleft/lesser.html)
 """
+
+from nlloc.grid import NLLocGrid
+
+
+def test_create_nlloc_grid():
+
+    dims = [100, 100, 100]
+    origin = [500, 1000, 10000]
+    spacing = [10, 5, 10]
+    phase = 'P'
+
+    grid = NLLocGrid('test', dims, origin, spacing, phase,
+                     seed=None, seed_label=None,
+                     grid_type='VELOCITY_METERS', grid_units='METER',
+                     float_type="FLOAT", resource_id=None)
+
+    assert grid.write()
