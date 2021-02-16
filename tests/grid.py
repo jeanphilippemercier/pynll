f02a398cd526145a7064fbb4994fa1a278aacd6c
jpmercier <jpmercier01@gmail.com>
2021-02-16 08:50:04 -0500

    progress on testing the velocity grid classes

--- tests/grid.py
+++ tests/grid.py
@@ -18,6 +18,7 @@
 """
 
 from nlloc.grid import NLLocGrid
+import matplotlib.pyplot as plt
 
 dims = [100, 100, 100]
 origin = [500, 1000, 10000]
@@ -42,3 +43,34 @@ def test_read_nlloc_grid():
     new_grid = NLLocGrid.from_file(base_name, path='.', float_type='FLOAT')
 
     assert new_grid == grid
+
+
+def test_layered_model():
+
+    from nlloc.grid import (ModelLayer, LayeredModel)
+
+    # The origin is the lower left corner
+    origin = [650200, 4766170, -500]
+    dimensions = [100, 101, 68]
+    spacing = [25, 25, 25]
+
+    z = [1168, 459, -300, -500]
+    vp_z = [4533, 5337, 5836, 5836]
+    vs_z = [2306, 2885, 3524, 3524]
+
+    layered_model = LayeredModel()
+    for (z_, vp) in zip(z, vp_z):
+        layer = ModelLayer(z_, vp)
+        layered_model.add_layer(layer)
+
+    z_bottom = origin[2]
+    z_top = origin[2] + spacing[2] * dimensions[2]
+    z_interp, v_interp = layered_model.gen_1d_model(z_bottom, z_top,
+                                                    spacing[2])
+
+    print(z_interp, v_interp)
+
+    assert True
+
+
+
